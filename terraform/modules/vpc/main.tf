a81b6740797ff746a1080e1ccd0f8050c843a8bf
saiyedin786 <saiyedin786@gmail.com>
2026-04-21 18:27:17 +0530

    terraform updated 3

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,9 +1,16 @@
+# Get AZs
 data "aws_availability_zones" "available" {}
 
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Public Subnets (2 AZs)
 resource "aws_subnet" "public" {
   count = 2
 
-  vpc_id = aws_vpc.main.id
+  vpc_id     = aws_vpc.main.id
   cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
 
   availability_zone = data.aws_availability_zones.available.names[count.index]
