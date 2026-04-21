5793e2f7839aaf3f7f627f12999e4c12cf8fcee5
saiyedin786 <saiyedin786@gmail.com>
2026-04-21 18:18:34 +0530

    terraform updated 1

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -5,3 +5,12 @@ variable "cluster_role_arn" {
 variable "subnet_ids" {
   type = list(string)
 }
+
+resource "aws_eks_cluster" "eks" {
+  name     = "shopnow-eks"
+  role_arn = var.cluster_role_arn
+
+  vpc_config {
+    subnet_ids = var.subnet_ids
+  }
+}
